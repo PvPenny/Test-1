3e34e742edee6a0247cce0c2a13bbf072923f79b
Alex Jey <alex.jey.2012@gmail.com>
2015-05-07 13:01:27 +0300

    Revert "Revert "Lab 17""
    This reverts commit bc54e2f28bf89116d7fb310dc4736ac8bb1350fb.

---
+++ WindowsFormsApplication17/WindowsFormsApplication17/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication17
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+           
+            InitializeComponent();
+           
+          
+        }
+        public string strTB;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+            strTB = this.maskedTextBox1.Text;
+
+           
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+            Form1 main = this.Owner as Form1;
+            maskedTextBox1.Mask = "00/00/0000           "+"0000"+"°С";
+
+           
+               this.maskedTextBox1.Text = main.strLB; ;
+          
+            
+               
+       
+           
+
+        }
+    }
+}
